f93efbda536c6051da6b1b32dc20b9ce7b5b06ea
Sonia Sandler <ssandler@redhat.com>
2025-01-21 09:24:56 -0500

    chore: test 4 Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/ui/src/lib/dropdown/Dropdown.svelte
+++ packages/ui/src/lib/dropdown/Dropdown.svelte
@@ -229,8 +229,9 @@ function onWindowClick(e: Event): void {
     </div>
   {/if}
 
+  <input name={name} value={value} type="hidden" aria-label="hidden input"/>
 
-  <select use:buildOptions class="hidden" name={name} bind:value={value}>
+  <select use:buildOptions class="hidden" bind:value={value}>
     {@render children?.()}
   </select>
 </div>
--- tests/playwright/src/specs/kubernetes.spec.ts
+++ tests/playwright/src/specs/kubernetes.spec.ts
@@ -68,7 +68,10 @@ test.beforeAll(async ({ runner, welcomePage, page, navigationBar }) => {
   }
 
   if (process.env.GITHUB_ACTIONS && process.env.RUNNER_OS === 'Linux') {
-    await createKindCluster(page, CLUSTER_NAME, false, CLUSTER_CREATION_TIMEOUT, { useIngressController: false });
+    await createKindCluster(page, CLUSTER_NAME, false, CLUSTER_CREATION_TIMEOUT, {
+      useIngressController: false,
+      providerType: 'podman',
+    });
   } else {
     await createKindCluster(page, CLUSTER_NAME, true, CLUSTER_CREATION_TIMEOUT);
   }

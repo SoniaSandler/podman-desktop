728b87536de43871f50aedbd198077d943000c9b
Sonia Sandler <ssandler@redhat.com>
2025-10-02 20:05:16 -0400

    chore: add networks list Signed-off-by: Sonia Sandler <ssandler@redhat.com>

---
+++ packages/renderer/src/lib/network/NetworkColumnActions.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import NetworkActions from './NetworkActions.svelte';
+import type { NetworkInfoUI } from './NetworkInfoUI';
+
+export let object: NetworkInfoUI;
+</script>
+
+<NetworkActions volume={object} on:update />
